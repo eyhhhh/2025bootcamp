17e05008347d718f578f77a9f65d3fd45528689e
eyhhhh <silver807@pusan.ac.kr>
2025-02-04 15:51:22 +0900

    002 가상환경 생성

--- blog-project/main.py
+++ blog-project/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
 from dataclasses import dataclass
+import time
 
 # 서버 생성
 app = FastAPI()
@@ -18,7 +19,28 @@ class Post:
   body: str
   created_at: int
   published: bool
+
+@dataclass
+class AuthLoginReq:
+  login_id: str
+  password: str
   
+@dataclass
+class AuthResponse:
+  jwt_token: str | None=None 
+  err_msg: str | None=None
+
 @app.post('/auth/signup')
-def signup():
+def signup(user: User)->AuthResponse:
+  return AuthResponse(
+    jwt_token="dsdkfdkfj"
+  )
+
+@app.post('/auth/signin')
+def signin(user: AuthLoginReq)->AuthResponse:
+  return AuthResponse(
+    jwt_token= "aaaa"
+  )
   
+@app.get('/posts')
+def get_posts():
